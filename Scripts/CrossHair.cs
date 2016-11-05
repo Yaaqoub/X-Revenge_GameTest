5b57ccd5455496ddbda3f36d6b4a5e56f44ea6b5
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2016-11-05 12:34:45 +0100

    Create CrossHair.cs

---
+++ Scripts/CrossHair.cs
@@ -0,0 +1,33 @@
+/* YAAQOUB SEMLALI ;
+ * semlali.yaaqoub@gmail.com ;
+ * GameName : XRevenge ( FPS Game ) | Final Year Project ;
+ */
+using UnityEngine;
+using System.Collections;
+
+public class CrossHair : MonoBehaviour {
+    public Texture crosshairTexture;
+    
+    private bool crosshairActivate;
+    private Rect crosshairRect;
+    
+	void Start () {
+        crosshairActivate = false;
+
+        float crosshairSize = Screen.width * 0.1f;
+        crosshairRect = new Rect(Screen.width / 1.92f - crosshairSize / 1.92f, Screen.height / 1.75f - crosshairSize / 1.52f, crosshairSize, crosshairSize);
+	}
+	
+  void Update() {
+      if (Input.GetKeyDown(KeyCode.C)) {
+            crosshairActivate = !crosshairActivate;
+      }
+    }
+
+	void OnGUI () {
+        if (crosshairActivate == true)
+        {
+            GUI.DrawTexture(crosshairRect, crosshairTexture);
+        }
+	}
+}
