920f8bf83fcea1770e522c9f29d09c7e7cd7fd97
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2016-11-05 12:53:43 +0100

    Create ScoreManager.cs

---
+++ Scripts/ScoreManager.cs
@@ -0,0 +1,32 @@
+/* YAAQOUB SEMLALI ;
+ * semlali.yaaqoub@gmail.com ;
+ * GameName : XRevenge ( FPS Game ) | Final Year Project ;
+ */
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int kills;        // The player's score.
+
+
+    Text text;                      // Reference to the Text component.
+
+
+    void Awake()
+    {
+        // Set up the reference.
+        text = GetComponent<Text>();
+
+        // Reset the score.
+        kills = 0;
+    }
+
+
+    void Update()
+    {
+        // Set the displayed text to be the word "Score" followed by the score value.
+        text.text = "Kills: " + kills;
+    }
+}
