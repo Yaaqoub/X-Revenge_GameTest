315ea05825547d653d2683f7d3e092972cc73934
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2016-11-05 12:46:46 +0100

    Create MouseLockVisible.cs

---
+++ Scripts/MouseLockVisible.cs
@@ -0,0 +1,32 @@
+/* YAAQOUB SEMLALI ;
+ * semlali.yaaqoub@gmail.com ;
+ * GameName : XRevenge ( FPS Game ) | Final Year Project ;
+ */
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MouseLockVisible : MonoBehaviour
+{
+    public Toggle pauseMenuTopCorner;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Time.timeScale == 1)
+        {
+            pauseMenuTopCorner.enabled = false;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            pauseMenuTopCorner.enabled = true;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
