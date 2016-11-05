48328b22eb80d543b0763bc093d22a835efab5bc
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2016-11-05 13:08:22 +0100

    Update AnimatorController.cs

--- Scripts/AnimatorController.cs
+++ Scripts/AnimatorController.cs
@@ -1,7 +1,8 @@
-/* YAAQOUB SEMLALI ;
+/**
+ * YAAQOUB SEMLALI & YASSINE CHETOUANE ;
  * semlali.yaaqoub@gmail.com ;
  * GameName : XRevenge ( FPS Game ) | Final Year Project ;
- */
+ **/
  
 using UnityEngine;
 using System.Collections;
