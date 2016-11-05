063f893b74c5da3cdcd97bbf4944e7139fcbdc66
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2016-11-05 12:46:24 +0100

    Create MouseController.cs

---
+++ Scripts/MouseController.cs
@@ -0,0 +1,29 @@
+/* YAAQOUB SEMLALI ;
+ * semlali.yaaqoub@gmail.com ;
+ * GameName : XRevenge ( FPS Game ) | Final Year Project ;
+ */
+using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour
+{
+    public float mouseSensitivity = 6.0f;
+    public float upDownRange = 60;
+
+    public GameObject fpsCamera;
+
+    private float verticalRotation = 0;
+
+    void Update()
+    {
+        float rotationLeftRight = Input.GetAxis("Mouse X") * mouseSensitivity;
+
+        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        transform.Rotate(0, rotationLeftRight, 0);
+
+        verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+
+        fpsCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+    }
+}
