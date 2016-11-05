4a3db7f72db87f85200b06ae139b4ab0c7d97aaf
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2016-11-05 12:42:01 +0100

    Create EnemyRobotTrigger.cs

---
+++ Scripts/EnemyRobotTrigger.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyRobotTrigger : MonoBehaviour 
+{
+    public GameObject[] enemy;
+    public EnemyHealth[] enemyIsDead;
+    public EnemyHealthRobot[] enemyRobotIsDead;
+
+    bool getInTheDoorRobot = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("U pass by");
+        getInTheDoorRobot = true;
+    }
+
+    void Update()
+    {
+        if (getInTheDoorRobot == true)
+        {
+            enemy[0].SetActive(true);
+
+            if (enemyIsDead[0].currentHealth <= 0)
+            {
+                enemy[1].SetActive(true);
+            }
+        }
+        if(RobotWarning.warActive == true)
+        {
+            enemy[2].SetActive(true);
+            enemy[3].SetActive(true);
+
+            if (enemyRobotIsDead[0].currentHealth <= 0 && enemyRobotIsDead[1].currentHealth <= 0)
+            {
+                enemy[4].SetActive(true);
+            }
+
+        }
+    }
+}
