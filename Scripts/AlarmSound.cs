418654678824fb3a5fca1946fdfe2776b5ff3f00
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2016-11-05 13:06:24 +0100

    Update AlarmSound.cs

--- Scripts/AlarmSound.cs
+++ Scripts/AlarmSound.cs
@@ -1,3 +1,8 @@
+/**
+ * YAAQOUB SEMLALI & YASSINE CHETOUANE ;
+ * semlali.yaaqoub@gmail.com ;
+ * GameName : XRevenge ( FPS Game ) | Final Year Project ;
+ **/
 using UnityEngine;
 using System.Collections;
 
@@ -12,12 +17,10 @@ public class AlarmSound : MonoBehaviour {
     public EnemyMovementMax enemy1Movement;
 
     private int destPoint = 0;
-
     private AudioSource alarmSound;
-
     public static bool alarmWorking = false;
 
-    void Start (){
+    void Start () {
         alarmSound = GetComponent<AudioSource>();
         //agent.autoBraking = false;
         anim.Play("idle");
@@ -33,30 +36,29 @@ public class AlarmSound : MonoBehaviour {
             anim.Play("punch");
             enemy1Anim.Play("punch");
         }
-        else{
+        else {
             enemy1Movement.enabled = true;
 
             float enemyAlarmDistance = Vector3.Distance(alarm.position, enemy.position);
 
             //Debug.Log(enemyAlarmDistance);
 
-            if (alarmWorking == false)
-            {
+            if (alarmWorking == false) {
                 agent.acceleration = 8f;
                 anim.Play("run");
                 agent.SetDestination(alarm.transform.position);
                 //agent.destination = alarm.position;
-                if (enemyAlarmDistance < 2f){
+                if (enemyAlarmDistance < 2f) {
                     if (!alarmSound.isPlaying && OnOffAlarm.aIsPressed == false){
                         alarmSound.Play();
                         alarmWorking = true;
                     }
                 }
             }
-            else{
+            else {
                 agent.SetDestination(player.transform.position);
 
-                if (agent.remainingDistance > 1.8f && !PlayerHealth.isDead){
+                if (agent.remainingDistance > 1.8f && !PlayerHealth.isDead) {
                     anim.Play("run");
                     agent.acceleration = 8;
                 }
