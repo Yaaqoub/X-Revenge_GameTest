185d870270f709924025e59038d0bd64477caaa4
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2016-11-05 12:51:26 +0100

    Create NavMeshTest.cs

---
+++ Scripts/NavMeshTest.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class NavMeshTest : MonoBehaviour 
+{
+    public Transform player;
+    public Transform enemy;
+    public Animation anim;
+
+    NavMeshAgent agent;
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        float enemyPlayerDistance = Vector3.Distance(player.position, enemy.position);
+
+        if (enemyPlayerDistance > 4)
+        {
+            agent.destination = player.position;
+            anim.Play("walk");
+        }
+        
+    }
+}
