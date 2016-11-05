1cbe9470aa7a1c55da4b9a3ef117793a4c99d971
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2016-11-05 12:39:34 +0100

    Create EnemyHealth.cs

---
+++ Scripts/EnemyHealth.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 100;            // The amount of health the enemy starts the game with.
+    public int currentHealth;                   // The current health the enemy has.
+    public int scoreValue = 1;                 // The amount added to the player's score when the enemy dies.
+    public AudioClip deathClip;                 // The sound to play when the enemy dies.
+
+
+    Animation anim;                              // Reference to the animator.
+    AudioSource enemyAudio;                     // Reference to the audio source.
+    CapsuleCollider capsuleCollider;            // Reference to the capsule collider.
+    bool isDead;                                // Whether the enemy is dead.
+
+    EnemyMovementMax enemyMove;
+    NavMeshAgent nav;
+    void Awake()
+    {
+        // Setting up the references.
+        anim = GetComponent<Animation>();
+        enemyAudio = GetComponent<AudioSource>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+
+        nav = GetComponent<NavMeshAgent>();
+        enemyMove = GetComponent<EnemyMovementMax>();
+        // Setting the current health when the enemy first spawns.
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        // If the enemy is dead...
+        if (isDead)
+        {
+            // ... no need to take damage so exit the function.
+            return;
+        }
+            
+
+        // Play the hurt sound effect.
+        enemyAudio.Play();
+
+        // Reduce the current health by the amount of damage sustained.
+        currentHealth -= amount;
+
+        // If the current health is less than or equal to zero...
+        if (currentHealth <= 0)
+        {
+            // ... the enemy is dead.
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        nav.enabled = false;
+        enemyMove.enabled = false;
+        // The enemy is dead.
+        isDead = true;
+
+        // Turn the collider into a trigger so shots can pass through it.
+        capsuleCollider.isTrigger = true;
+        
+        // Tell the animator that the enemy is dead.
+        anim.Play("death", PlayMode.StopAll);
+        
+        // Change the audio clip of the audio source to the death clip and play it (this will stop the hurt clip playing).
+        enemyAudio.clip = deathClip;
+        enemyAudio.Play();
+
+        // Find and disable the Nav Mesh Agent.
+        GetComponent<NavMeshAgent>().enabled = false;
+
+        // Find the rigidbody component and make it kinematic (since we use Translate to sink the enemy).
+        GetComponent<Rigidbody>().isKinematic = true;
+
+        // Increase the score by the enemy's score value.
+        ScoreManager.kills += scoreValue;
+
+        //Destroy(gameObject, 2f);
+    }
+}
