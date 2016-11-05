678cd7cf04958d1b24deedeb5073d2477f907cf2
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2016-11-05 12:31:49 +0100

    Create BulletCollision.cs

---
+++ Scripts/BulletCollision.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class BulletCollision : MonoBehaviour {
+    int basicBulletDmg = 1;
+    int damage;
+
+    void Start() {
+        if (this.gameObject.name == "Bullet_exp") {
+            damage = basicBulletDmg;
+        }
+    }
+
+    void OnCollisionEnter(Collision col) {
+        
+        if (col.gameObject.tag == "Enemy") {
+            //Debug.Log(col.gameObject.name + " is an enemy! You did " + damage + " damage!!");
+
+            Health enemyHealth = col.gameObject.GetComponent<Health>();
+            //enemyHealth.ModifyHealth(-damage);
+        }
+        Destroy(this.gameObject);
+    }
+    
+}
