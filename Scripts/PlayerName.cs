d1d05316fb14270ecfe22115b38048155eb67363
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2016-11-05 13:31:30 +0100

    Update PlayerName.cs

--- Scripts/PlayerName.cs
+++ Scripts/PlayerName.cs
@@ -1,3 +1,8 @@
+/**
+ * YAAQOUB SEMLALI & YASSINE CHETOUANE ;
+ * semlali.yaaqoub@gmail.com ;
+ * GameName : XRevenge ( FPS Game ) | Final Year Project ;
+ **/
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
