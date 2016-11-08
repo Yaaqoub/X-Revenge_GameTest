a70f44e557d8eb8ae9bcbdce8d6d2892261f0db6
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2016-11-08 07:15:41 +0100

    Create LoadUrlLevel.cs

---
+++ Menu/Scripts/LoadUrlLevel.cs
@@ -0,0 +1,15 @@
+/**
+ * YAAQOUB SEMLALI & YASSINE CHETOUANE ;
+ * semlali.yaaqoub@gmail.com ;
+ * GameName : XRevenge ( FPS Game ) | Final Year Project ;
+ **/
+using UnityEngine;
+using System.Collections;
+
+public class LoadUrlLevel : MonoBehaviour
+{
+    public void LoadURL(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
