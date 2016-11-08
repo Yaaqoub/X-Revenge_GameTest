a722328f571399d11fe94ef4257ffdc656e250c0
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2016-11-08 07:19:16 +0100

    Create ReturnToMainMenu.cs

---
+++ Menu/Scripts/ReturnToMainMenu.cs
@@ -0,0 +1,44 @@
+/**
+ * YAAQOUB SEMLALI & YASSINE CHETOUANE ;
+ * semlali.yaaqoub@gmail.com ;
+ * GameName : XRevenge ( FPS Game ) | Final Year Project ;
+ **/
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMainMenu : MonoBehaviour
+{
+    private bool m_Levelloaded;
+
+
+    public void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+
+    private void OnLevelWasLoaded(int level)
+    {
+        m_Levelloaded = true;
+    }
+
+
+    private void Update()
+    {
+        if (m_Levelloaded)
+        {
+            Canvas component = gameObject.GetComponent<Canvas>();
+            component.enabled = false;
+            component.enabled = true;
+            m_Levelloaded = false;
+        }
+    }
+
+
+    public void GoBackToMainMenu()
+    {
+        Debug.Log("going back to main menu");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
