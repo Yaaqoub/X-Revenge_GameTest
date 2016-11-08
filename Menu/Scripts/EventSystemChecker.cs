ff2a12370d186378848a2e6dc6a83624b68b102e
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2016-11-08 07:15:08 +0100

    Create EventSystemChecker.cs

---
+++ Menu/Scripts/EventSystemChecker.cs
@@ -0,0 +1,25 @@
+/**
+ * YAAQOUB SEMLALI & YASSINE CHETOUANE ;
+ * semlali.yaaqoub@gmail.com ;
+ * GameName : XRevenge ( FPS Game ) | Final Year Project ;
+ **/
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventSystemChecker : MonoBehaviour
+{
+    //public GameObject eventSystem;
+
+	void Awake ()
+	{
+	    if(!FindObjectOfType<EventSystem>())
+        {
+           //Instantiate(eventSystem);
+            GameObject obj = new GameObject("EventSystem");
+            obj.AddComponent<EventSystem>();
+            obj.AddComponent<StandaloneInputModule>().forceModuleActive = true;
+        }
+	}
+}
